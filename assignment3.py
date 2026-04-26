2e8aab2256481c369bd222148ed832b7d8a7c2bf
sahilkiranpawar11-a11y <sahilkiranpawar11@gmail.com>
2026-04-26 12:32:19 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,35 @@
+student = {
+"roll_no": 101,
+"name": "Amit",
+"marks": 85
+}
+
+print("Dictionary:", student)
+print("Name:", student["name"])
+print("Marks:", student.get("marks"))
+print()
+
+#2. Update Dictionary
+print("Update Dictionary")
+student["marks"] = 90
+student["grade"] = "A"
+print("Updated Dictionary:", student)
+
+#3. Removing Elements
+print("Removing Elements")
+removed_value = student.pop("grade")
+print("Removed Value:", removed_value)
+print("After Removing 'grade':", student)
+
+student.popitem()
+print("After popitem():", student)
+
+#4. Merging Dictionaries
+print("Merging Dictionaries")
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+
+merged_dict = dict1 | dict2
+print("First Dictionary:", dict1)
+print("Second Dictionary:", dict2)
+print("Merged Dictionary:", merged_dict)
